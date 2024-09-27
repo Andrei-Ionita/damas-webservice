38c2d191bd68ea7107da710a0683cab5afd1d91b
Andrei Ionita <andrei.ionita@mynexte.com>
2024-09-27 19:53:03 +0300

    Creating the Generation Schedule for 28.09.2024

--- app.py
+++ app.py
@@ -137,23 +137,12 @@ def get_generation_schedule_manually():
 
 def create_tomorrows_generation_schedule():
     intervals = []
-<<<<<<< HEAD
     base_time = datetime.strptime("2024-09-27T21:00:00Z", "%Y-%m-%dT%H:%M:%SZ")
-=======
-    base_time = datetime.strptime("2024-09-26T21:00:00Z", "%Y-%m-%dT%H:%M:%SZ")
->>>>>>> ffa10995e02a0e57ec31cd8412c2a9764f8f59ee
     for i in range(96):
         start_time = base_time + timedelta(minutes=15 * i)
         end_time = start_time + timedelta(minutes=15)
         hour = (start_time.hour + 3) % 24  # Adjust for EET (UTC+3 in summer)
-        
-<<<<<<< HEAD
-        if 6 <= hour <=11:
-            power = 4
-        elif 17 <= hour <= 24:
-=======
         if 7 <= hour <=24:
->>>>>>> ffa10995e02a0e57ec31cd8412c2a9764f8f59ee
             power = 4
         # elif 16 <= hour < 19:
         #     power = 8.6
@@ -185,16 +174,16 @@ def create_tomorrows_generation_schedule():
 
 def create_2days_ahead_generation_schedule():
     intervals = []
-    base_time = datetime.strptime("2024-09-25T21:00:00Z", "%Y-%m-%dT%H:%M:%SZ")
+    base_time = datetime.strptime("2024-09-27T21:00:00Z", "%Y-%m-%dT%H:%M:%SZ")
     for i in range(96):
         start_time = base_time + timedelta(minutes=15 * i)
         end_time = start_time + timedelta(minutes=15)
         hour = (start_time.hour + 3) % 24  # Adjust for EET (UTC+3 in summer)
         
-        if 7 <= hour <= 24:
+        if 6 <= hour <= 11:
             power = 4
-        # elif 17 <= hour <= 24:
-        #     power = 4
+        elif 17 <= hour <= 24:
+            power = 4
         # elif 16 <= hour < 19:
         #     power = 8.6
         else:
@@ -365,13 +354,9 @@ def refresh_data(date_from, date_to, previous_order_count):
     # Fetch generation schedule
     response_schedule = get_generation_schedule_manually()
     current_date = datetime.now().date()
-<<<<<<< HEAD
-    if current_date == datetime(2024, 9, 28).date():
-=======
     if current_date == datetime(2024, 9, 27).date():
->>>>>>> ffa10995e02a0e57ec31cd8412c2a9764f8f59ee
         generation_schedule = create_tomorrows_generation_schedule()
-    elif current_date == datetime(2024, 9, 26).date():
+    elif current_date == datetime(2024, 9, 28).date():
         generation_schedule = create_2days_ahead_generation_schedule()
     else:
         generation_schedule = []  # Replace with your usual generation schedule fetching logic
