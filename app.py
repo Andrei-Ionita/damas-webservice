9bdb517e0f5aa81d648dbff4114195fcf13295da
Andrei Ionita <andrei.ionita@mynexte.com>
2024-11-26 18:17:58 +0200

    Creating the Generation Schedule for 27.11.2024

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def create_tomorrows_generation_schedule():
 
 def create_2days_ahead_generation_schedule():
     intervals = []
-    base_time = datetime.strptime("2024-11-24T22:00:00Z", "%Y-%m-%dT%H:%M:%SZ")
+    base_time = datetime.strptime("2024-11-26T22:00:00Z", "%Y-%m-%dT%H:%M:%SZ")
     for i in range(96):
         start_time = base_time + timedelta(minutes=15 * i)
         end_time = start_time + timedelta(minutes=15)
@@ -341,7 +341,7 @@ def refresh_data(date_from, date_to, previous_order_count):
     current_date = datetime.now().date()
     if current_date == datetime(2024, 11, 26).date():
         generation_schedule = create_tomorrows_generation_schedule()
-    elif current_date == datetime(2024, 11, 25).date():
+    elif current_date == datetime(2024, 11, 27).date():
         generation_schedule = create_2days_ahead_generation_schedule()
     else:
         generation_schedule = []  # Replace with your usual generation schedule fetching logic
