a1924ec0005c0698c79ab37054330c1d4aaafb26
Andrei Ionita <andrei.ionita@mynexte.com>
2024-12-03 18:14:37 +0200

    Creating the Generation Schedule for 04.12.2024

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ def get_generation_schedule_manually():
 
 def create_tomorrows_generation_schedule():
     intervals = []
-    base_time = datetime.strptime("2024-12-01T22:00:00Z", "%Y-%m-%dT%H:%M:%SZ")
+    base_time = datetime.strptime("2024-12-03T22:00:00Z", "%Y-%m-%dT%H:%M:%SZ")
     for i in range(96):
         start_time = base_time + timedelta(minutes=15 * i)
         end_time = start_time + timedelta(minutes=15)
@@ -356,7 +356,7 @@ def refresh_data(date_from, date_to, previous_order_count):
     response_schedule = get_generation_schedule(date_from, date_to)
 
     current_date = datetime.now().date()
-    if current_date == datetime(2024, 12, 2).date():
+    if current_date == datetime(2024, 12, 4).date():
         generation_schedule = create_tomorrows_generation_schedule()
     elif current_date == datetime(2024, 12, 3).date():
         generation_schedule = create_2days_ahead_generation_schedule()
